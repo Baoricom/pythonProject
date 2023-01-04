8cf311570f29c1791d839ae1595c28ce0c077b5d
Baoricom <121966095+Baoricom@users.noreply.github.com>
2023-01-04 23:12:22 +0600

    Delete Learn directory

--- Learn/word_replace.py
+++
@@ -1,6 +0,0 @@
-def replace():
-    str = "Hello, my name is John"
-    word_replace = input("Enter the word to replace:")
-    word_replacement = input("Enter the word to replacement:")
-    print(str.replace(word_replace,word_replacement))
-replace()
